bb6bd2cd41994c798f495a6e66b73e3e6d85a008
carlosriosacevedo17 <48395676+carlosriosacevedo17@users.noreply.github.com>
2019-04-03 09:54:35 -0600

    Add files via upload

--- FACULTAD/js/javascript6.js
+++ FACULTAD/js/javascript6.js
@@ -1,6 +1,13 @@
 function displayDate() {
 document.getElementById("fechaFooter").innerHTML=Date();
 }
-function displayFecha(y){
-	alert("El año es:"+y);
+function displayFecha(y,m,d){
+	var cadena=y+"/"+m+"/"+d;
+document.getElementById("fecha").value=cadena;
+}
+function muestraEdad(){
+ var f_actual=document.getElementById("f_actual").value;
+ var f_nac=document.getElementById("f_nac").value;
+ alert(f_actual);
+ alert(f_nac);
 }
